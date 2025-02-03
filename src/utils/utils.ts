226c17ce7ecf3839796e6b78e73c2c32a730b78f
Khaybee <kachieosuji11@gmail.com>
2025-02-03 13:22:18 +0100

    update

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -9,9 +9,10 @@ export function errorHandler(res: Response, input: any, code: number = 500) {
   }
 
 export function isArmstrongNumber(num: number): boolean {
+    const positiveNum = Math.abs(num);
     let sum = 0;
-    let temp = num;
-    const power = Math.floor(Math.log10(num)) + 1;
+    let temp = positiveNum;
+    const power = Math.floor(Math.log10(positiveNum)) + 1;
 
     while (temp > 0) {
         const digit = temp % 10;
@@ -19,7 +20,7 @@ export function isArmstrongNumber(num: number): boolean {
         temp = Math.floor(temp / 10);
     }
 
-    return sum === num;
+    return sum === positiveNum;
 }
 
 export function isPrime(num: number): boolean {
@@ -51,17 +52,28 @@ export function isPerfectNumber(num: number): boolean {
 }
 
 export function sumDigits(num: number) {
-    const absoluteValue = Math.abs(num);
-    const sum = absoluteValue
+    const sum = Math.abs(num)
         .toString()
         .split('')
         .reduce((sum, digit) => sum + +digit, 0);
 
-    // If the number is negative, we negate the sum
-    const finalSum = num < 0 ? -sum : sum;
-
-    const isEven = finalSum % 2 === 0;
-    const isOdd = !isEven;
-
-    return { sum: finalSum, is_even: isEven, is_odd: isOdd };
+    const isEven = sum % 2 === 0;
+    return { sum, is_even: isEven, is_odd: !isEven };
 }
+
+
+// export function sumDigits(num: number) {
+//     const absoluteValue = Math.abs(num);  // Work with the absolute value for digit extraction
+//     const sum = absoluteValue
+//         .toString()
+//         .split('')
+//         .reduce((sum, digit) => sum + +digit, 0);
+
+//     // If the number is negative, we negate the sum
+//     const finalSum = num < 0 ? -sum : sum;
+
+//     const isEven = finalSum % 2 === 0;
+//     const isOdd = !isEven;
+
+//     return { sum: finalSum, is_even: isEven, is_odd: isOdd };
+// }
